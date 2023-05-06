978496c1044144dcfab95d115c59863fff254e2e
Julian Fuentes <fuentes.julian@hotmail.com>
2023-05-06 14:46:58 -0300

    Agregando try/catch y condicionales al product router para manejar errores de servidor y de usuario

--- Proyecto Final/src/routes/products.router.js
+++ Proyecto Final/src/routes/products.router.js
@@ -6,33 +6,49 @@ const productsManager = new ItemManager("./src/database/productos.json");
 
 router
   .get("/", async (req, res) => {
-    const allProducts = await productsManager.getAll();
-    const limit = req.query.limit;
-    const products = limit ? allProducts.slice(0, limit) : allProducts;
-    res.status(200).send(products);
+    try {
+      const allProducts = await productsManager.getAll();
+      const limit = req.query.limit;
+      const products = limit ? allProducts.slice(0, limit) : allProducts;
+      res.status(200).send(products);
+    } catch (error) {
+      res.status(500).send({ error: error.message });
+    }
   })
   .get("/:pid", async (req, res) => {
-    const pid = req.params.pid;
-    const product = await productsManager.getById(pid);
-    res.status(200).send(product);
+    try {
+      const pid = req.params.pid;
+      const product = await productsManager.getById(pid);
+      if (product === undefined) {
+        res.status(400).send({ error: "Parametros incorrectos" });
+      } else {
+        res.status(200).send(product);
+      }
+    } catch (error) {
+      res.status(500).send({ error: error.message });
+    }
   })
   .post("/", async (req, res) => {
     const { title, description, code, price, stock, category, thumbnails } =
       req.body;
 
     if (title && description && code && price && stock && category) {
-      const productData = {
-        title,
-        description,
-        code,
-        price,
-        stock,
-        status: true,
-        category,
-        thumbnails: thumbnails ?? [],
-      };
-      const product = await productsManager.createOne(productData);
-      res.status(201).send(product);
+      try {
+        const productData = {
+          title,
+          description,
+          code,
+          price,
+          stock,
+          status: true,
+          category,
+          thumbnails: thumbnails ?? [],
+        };
+        const product = await productsManager.createOne(productData);
+        res.status(201).send(product);
+      } catch (error) {
+        res.status(500).send({ error: error.message });
+      }
     } else {
       res.status(400).send({ error: "Faltan parametros" });
     }
@@ -51,27 +67,34 @@ router
     } = req.body;
 
     if (title && description && code && price && stock && category) {
-      let productData = {
-        title,
-        description,
-        code,
-        price,
-        stock,
-        category,
-        thumbnails: thumbnails ?? [],
-      };
-      if (status !== undefined) productData = { ...productData, status };
-
-      const product = await productsManager.updateOne(pid, productData);
-      res.status(200).send(`Modifico el producto id ${pid}`);
+      try {
+        let productData = {
+          title,
+          description,
+          code,
+          price,
+          stock,
+          category,
+          thumbnails: thumbnails ?? [],
+        };
+        if (status !== undefined) productData = { ...productData, status };
+        const product = await productsManager.updateOne(pid, productData);
+        res.status(200).send(`Modifico el producto id ${pid}`);
+      } catch (error) {
+        res.status(500).send({ error: error.message });
+      }
     } else {
       res.status(400).send({ error: "Faltan parametros" });
     }
   })
   .delete("/:pid", async (req, res) => {
     const pid = req.params.pid;
-    await productsManager.deleteOne(pid);
-    res.status(200).send({ success: `Producto id ${pid} eliminado` });
+    try {
+      await productsManager.deleteOne(pid);
+      res.status(200).send({ success: `Producto id ${pid} eliminado` });
+    } catch (error) {
+      res.status(500).send({ error: error.message });
+    }
   });
 
 export default router;

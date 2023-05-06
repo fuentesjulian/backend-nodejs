27c43e56d9e179fd3b90ec962b73e643cecf397a
Julian Fuentes <fuentes.julian@hotmail.com>
2023-05-06 14:07:22 -0300

    Agregando try/catch y condicionales para manejar errores de servidor y de usuario

--- Proyecto Final/src/routes/carts.router.js
+++ Proyecto Final/src/routes/carts.router.js
@@ -7,32 +7,55 @@ const cartsManager = new ItemManager("./src/database/carrito.json");
 router
   .post("/", async (req, res) => {
     const cartData = { products: [] };
-    const cart = await cartsManager.createOne(cartData);
-    res.status(201).send(cart);
+    try {
+      const cart = await cartsManager.createOne(cartData);
+      res.status(201).send(cart);
+    } catch (error) {
+      res.status(500).send({ error: error.message });
+    }
   })
   .get("/:cid", async (req, res) => {
     const cid = req.params.cid;
-    const cart = await cartsManager.getById(cid);
-    const products = cart.products;
-    res.status(200).send(products);
+    try {
+      const cart = await cartsManager.getById(cid);
+      if (cart) {
+        const products = cart.products;
+        res.status(200).send(products);
+      } else {
+        res.status(400).send({ error: "Parametros incorrectos" });
+      }
+    } catch (error) {
+      res.status(500).send({ error: error.message });
+    }
   })
   .post("/:cid/product/:pid", async (req, res) => {
     const cid = req.params.cid;
     const pid = req.params.pid;
     const { quantity } = req.body;
     const cart = await cartsManager.getById(cid);
-    let products = cart.products;
-    const inCart = products.some((prod) => prod.product == pid);
-    if (inCart) {
-      products = products.map((prod) => {
-        if (prod.product == pid) return { ...prod, quantity };
-        return prod;
-      });
-    } else {
-      products.push({ product: pid, quantity });
+
+    try {
+      if (cart === undefined || quantity === undefined) {
+        res.status(400).send({ error: "Parametros incorrectos" });
+      } else {
+        let products = cart.products;
+        const inCart = products.some((prod) => prod.product == pid);
+        if (inCart) {
+          products = products.map((prod) => {
+            if (prod.product == pid) return { ...prod, quantity };
+            return prod;
+          });
+        } else {
+          products.push({ product: pid, quantity });
+        }
+        await cartsManager.updateOne(cid, { products });
+        res
+          .status(201)
+          .send(`Agrego cantidad de prod id ${pid} en el cart ${cid}`);
+      }
+    } catch (error) {
+      res.status(500).send({ error: error.message });
     }
-    await cartsManager.updateOne(cid, { products });
-    res.status(201).send(`Agrego cantidad de prod id ${pid} en el cart ${cid}`);
   });
 
 export default router;

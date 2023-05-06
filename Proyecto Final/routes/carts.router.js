b02633dba4da5db13384480820ece461819094f7
Julian Fuentes <fuentes.julian@hotmail.com>
2023-05-05 23:25:03 -0300

    Definiendo todas las rutas

--- Proyecto Final/routes/carts.router.js
+++ Proyecto Final/routes/carts.router.js
@@ -2,8 +2,18 @@ import { Router } from "express";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.status(200).send({ msg: "Probando get" });
-});
+router
+  .post("/", (req, res) => {
+    res.status(201).send("Creo un nuevo cart");
+  })
+  .get("/:cid", (req, res) => {
+    const cid = req.params.cid;
+    res.status(200).send(`Obtengo cart ${cid}`);
+  })
+  .post("/:cid/product/:pid", (req, res) => {
+    const cid = req.params.cid;
+    const pid = req.params.pid;
+    res.status(201).send(`Agrego cantidad de prod id ${pid} en el cart ${cid}`);
+  });
 
 export default router;

9c141a001cb40be4d7ea445e2d43e5e68f20af64
Julian Fuentes <fuentes.julian@hotmail.com>
2023-05-06 11:03:43 -0300

    Agregando funcionalidad al metodo post al cart router para crear carrito

--- Proyecto Final/src/routes/carts.router.js
+++ Proyecto Final/src/routes/carts.router.js
@@ -1,16 +1,20 @@
 import { Router } from "express";
+import ItemManager from "../database/ItemManager.js";
 
 const router = Router();
+const cartsManager = new ItemManager("./src/database/carrito.json");
 
 router
-  .post("/", (req, res) => {
-    res.status(201).send("Creo un nuevo cart");
+  .post("/", async (req, res) => {
+    const cartData = { products: [] };
+    const cart = await cartsManager.createOne(cartData);
+    res.status(201).send(cart);
   })
-  .get("/:cid", (req, res) => {
+  .get("/:cid", async (req, res) => {
     const cid = req.params.cid;
     res.status(200).send(`Obtengo cart ${cid}`);
   })
-  .post("/:cid/product/:pid", (req, res) => {
+  .post("/:cid/product/:pid", async (req, res) => {
     const cid = req.params.cid;
     const pid = req.params.pid;
     res.status(201).send(`Agrego cantidad de prod id ${pid} en el cart ${cid}`);

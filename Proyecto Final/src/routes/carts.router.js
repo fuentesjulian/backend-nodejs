0302050d86022486a4941bf492826ac4887ee090
Julian Fuentes <fuentes.julian@hotmail.com>
2023-06-11 01:04:20 -0300

    Eliminando archivos innecesarios (Filesystem)

--- Proyecto Final/src/routes/carts.router.js
+++ Proyecto Final/src/routes/carts.router.js
@@ -1,14 +1,9 @@
 // importo el router de express
 import { Router } from "express";
-// importo el item manager
-import ItemManager from "../database/ItemManager.js";
 // importo el carts controller
 import * as cartsController from "../controllers/carts.controllers.js";
 // creo mi router
 const router = Router();
-// instancio cartsManager y el productsManager con el path
-const cartsManager = new ItemManager("./src/database/carrito.json");
-const productsManager = new ItemManager("./src/database/productos.json");
 
 router
   .post("/", cartsController.createCart)

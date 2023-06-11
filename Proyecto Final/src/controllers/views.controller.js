6cc9d7b61080b1640e34bc65901cfd370e0d9eb2
Julian Fuentes <fuentes.julian@hotmail.com>
2023-06-11 18:45:45 -0300

    Agregando not found

--- Proyecto Final/src/controllers/views.controller.js
+++ Proyecto Final/src/controllers/views.controller.js
@@ -14,19 +14,30 @@ export const getAllProducts = async (req, res) => {
   res.render("products", { response: respObj });
 };
 export const getOneProduct = async (req, res) => {
-  const pid = req.params.pid;
-  const response = await productService.getById(pid);
-  const prod = asPOJO(response);
-  res.render("product", { ...prod });
+  try {
+    const pid = req.params.pid;
+    const response = await productService.getById(pid);
+    const prod = asPOJO(response);
+    res.render("product", { ...prod });
+  } catch (error) {
+    res.render("notfound");
+  }
 };
 export const getOneCart = async (req, res) => {
-  const cid = req.params.cid;
-  const response = await cartsService.getCart(cid);
-  const cartData = await response.populate("products.product")
-  const cart = asPOJO(cartData);
-  res.render("cart", { cart });
+  try {
+    const cid = req.params.cid;
+    const response = await cartsService.getCart(cid);
+    const cartData = await response.populate("products.product");
+    const cart = asPOJO(cartData);
+    res.render("cart", { cart });   
+  } catch (error) {
+    res.render("notfound")
+  }
+
 };
 export const createOneProduct = async (req, res) => {
   res.render("new");
 };
-export const notFound = (req, res) => {};
+export const notFound = (req, res) => {
+  res.render("notfound")
+};

0cb2083864224fe301cc345f10eead54dde9c5fe
Julian Fuentes <fuentes.julian@hotmail.com>
2023-06-17 23:54:56 -0300

    Agregando validacion de formulario directamente en javascript

---
+++ Entregable 5/public/js/login.js
@@ -0,0 +1,34 @@
+const loginForm = document.getElementById("loginForm");
+const errorHtml = document.getElementById("error");
+
+const renderError = (error) => {
+  errorHtml.innerHTML = `<div class="alert alert-danger" role="alert">${error}</div>`;
+};
+
+const redirect = () => {
+    loginForm.innerHTML = `<div>Login successful... redirecting!!!</div>`;
+  setTimeout(() => {
+    window.location.href = "/";
+  }, 3000);
+};
+
+const handleLogin = async (e) => {
+  errorHtml.innerHTML = "";
+  e.preventDefault();
+  const formData = new FormData(loginForm);
+  let dataObj = {};
+  formData.forEach((val, key) => (dataObj[key] = val));
+
+  const data = await fetch("http://localhost:8080/api/users/auth", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(dataObj),
+  });
+  const json = await data.json();
+  if (json.status === "success") redirect();
+  if (json.status === "error") renderError(json.payload);
+};
+
+loginForm.addEventListener("submit", handleLogin);
--- Entregable 5/src/routes/users.router.js
+++ Entregable 5/src/routes/users.router.js
@@ -19,10 +19,10 @@ usersRouter
   .post("/auth", async (req, res) => {
     try {
       const { email, password } = req.body;
-      if (!email || !password) throw new Error("missing");
+      if (!email || !password) throw new Error("user and email are required");
       const user = await userService.geyByEmail(email);
-      if (!user) throw new Error("noauth");
-      if (user.password !== password) throw new Error("noauth");
+      if (!user) throw new Error("invalid credentials");
+      if (user.password !== password) throw new Error("invalid credentials");
       let userObj = JSON.parse(JSON.stringify(user));
       if (
         userObj.email === "adminCoder@coder.com" &&
@@ -31,13 +31,9 @@ usersRouter
         userObj.role = "admin";
       delete userObj.password;
       req.session.user = userObj;
-      res.redirect("/");
+      res.status(201).send({ status: "success" });
     } catch (error) {
-      if (error.message === "missing" || error.message === "noauth") {
-        res.redirect(`/login?error=${error.message}`);
-      } else {
-        res.redirect(`/login?error=internal`);
-      }
+      res.status(400).send({ status: "error", payload: error.message });
     }
   })
   .post("/logout", async (req, res) => {

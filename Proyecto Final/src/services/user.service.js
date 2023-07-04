0478f630bfb0e964c80c145f103db22b8110b188
Julian <fuentes.julian@hotmail.com>
2023-07-04 18:24:55 -0300

    Agregando bcrypt para encriptar password

--- Proyecto Final/src/services/user.service.js
+++ Proyecto Final/src/services/user.service.js
@@ -1,9 +1,11 @@
 import User from "../models/User.js";
 import CustomError from "../utils/CustomError.utils.js";
+import { hashPassword } from "../utils/cipher.utils.js";
 
 export const register = async (userData) => {
   const user = await User.findOne({ email: userData.email });
   if (user) throw new CustomError(400, "User already exists");
+  userData.password = hashPassword(userData.password);
   const newUser = await User.create(userData);
   return newUser;
 };
